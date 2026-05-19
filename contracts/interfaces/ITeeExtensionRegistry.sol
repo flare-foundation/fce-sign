5dfe834c14cabcbeb53f757d1c4c957607b98e3e
JakobOpresnik <jakob.opresnik@gmail.com>
2026-05-19 12:50:58 +0200

    refactor: restructure Solidity contracts to new scaffold layout

--- contracts/interfaces/ITeeExtensionRegistry.sol
+++ contracts/interfaces/ITeeExtensionRegistry.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.6 <0.9;
 
+// TODO: Replace this minimal interface with the full import once flare-smart-contracts-v2
+// is published as a package:
+//   import { ITeeExtensionRegistry } from "flare-smart-contracts-v2/contracts/userInterfaces/tee/ITeeExtensionRegistry.sol";
 interface ITeeExtensionRegistry {
     struct TeeInstructionParams {
         bytes32 opType;
@@ -12,8 +15,8 @@ interface ITeeExtensionRegistry {
     }
 
     function sendInstructions(
-        address[] memory _teeIds,
-        TeeInstructionParams memory _instructionParams
+        address[] calldata _teeIds,
+        TeeInstructionParams calldata _instructionParams
     ) external payable returns (bytes32 _instructionId);
 
     function extensionsCounter() external view returns (uint256);
